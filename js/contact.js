e7a0d20408e9fb8f1c4a883b8f435ec5d22d30c0
Anthonyx2010 <Anthonyx2010@gmail.com>
2019-03-23 22:27:36 -0500

    Rest 3

--- js/contact.js
+++ js/contact.js
@@ -9,14 +9,14 @@ function validate () {
 
   var alertMsg = "";
 
-  // Name and one form of contact information (Email or Phone) should be filled in
+  //  form of contact information
   if (!(nameInput.value && (emailInput.value || phoneInput.value))) {
     if (nameInput.value) alertMsg+="\u2022 Please include at least one way to contact you (either an email or phone number)\n\n";
     else if (emailInput.value || phoneInput.value) alertMsg+="\u2022 Please enter your name\n\n";
     else alertMsg+="\u2022 Make sure you have entered your name and atleast one way to contact you\n\n";
   }
 
-  // If Reason for Inquiry's dropdown is selected to Other, make sure that the Additional Information is filled in.
+  // Reason for Inquiry's dropdown
   if (reasonInput.value == "other" && !additionalInput.value) {
     alertMsg+="\u2022 (Other) Please explain your reason for inquiring in the Additional Information section\n\n";
   }
